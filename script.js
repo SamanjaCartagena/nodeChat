b74eafc7dd932cf7a2313457391b8dcd706b4e8a
SamanjaCartagena <cart.samanja09@gmail.com>
2022-11-17 00:06:03 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const socket = io('http://localhost:3000')
+const messageForm = document.getElementById('send-container')
+
+
+socket.on('chat-message', data => {
+    console.log(data)
+})
+
+messageForm.addEventListener('submit',e =>{
+    e.preventDefault()
+})
